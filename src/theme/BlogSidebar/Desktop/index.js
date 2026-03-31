e697d2641e3dc60a0ea05e96cfc812a738eda8d1
안형준 <wns1265@gmail.com>
2026-04-01 00:12:02 +0900

    refactor: 웹 소켓 통신 성능개선 내용

--- src/theme/BlogSidebar/Desktop/index.js
+++ src/theme/BlogSidebar/Desktop/index.js
@@ -18,6 +18,7 @@ const CATEGORIES = [
       {label: 'Generic', to: '/blog/tags/generic'},
       {label: 'JDBC / DB', to: '/blog/tags/jdbc'},
       {label: 'Web 기초', to: '/blog/tags/web'},
+      {label: '비동기', to: '/blog/tags/async'},
     ],
   },
   {
